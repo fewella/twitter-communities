abcb129b5c470b4160aaa9179b95ff4a619a462b
Ajay Fewell <afewell2@illinois.edu>
2021-03-12 15:17:17 -0500

    deals with rate limiting, gathers data over time and places into data directory

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import time
 
 def auth():
     return os.environ.get("BEARER_TOKEN")
@@ -52,15 +53,21 @@ def get_uids(usernames_as_list):
     url = create_username_url(usernames_as_str)
     headers = create_headers(bearer_token)
     r = connect_to_endpoint(url, headers)
+    
+    print(r)
+    if 'errors' in r:
+        return -1
 
     ids = []
     for u in r['data']:
         ids.append(u['id'])
-    
+    if len(ids) == 1:
+        ids = ids[0]
+
     return ids
 
 
-def retrieve_all_followers(uid):
+def retrieve_sample_followers(uid):
     bearer_token = auth()
     url = create_uid_url(uid)
     headers = create_headers(bearer_token)
@@ -69,18 +76,21 @@ def retrieve_all_followers(uid):
     
     followers = set()
 
-    while True:
-        r = connect_to_endpoint(url, headers, params)
+    for i in range(15):
+        try:
+            r = connect_to_endpoint(url, headers, params)
+        except:
+            time.sleep(16 * 60)
+            r = connect_to_endpoint(url, headers, params)
+
         for u in r['data']:
             followers.add(u['id'])
         
-        print(r['meta'])
         if 'next_token' in r['meta']:
             params['pagination_token'] = r['meta']['next_token']
         else:
             break
     
-    print(len(followers))
     return followers
     
 
@@ -101,14 +111,32 @@ def compare_followers(uid1, uid2):
     return degree_of_similarity
 
 
+def retrieve_input_followers(filename):
+    f = open(filename, "r")
+    usernames = []
+    for line in f:
+        usernames.append(line.rstrip())
+    f.close()
+
+    for username in usernames:
+        print("retrieving", username)
+        uid = get_uids([username])
+        if uid == -1:
+            print("ERROR: could not find uid for", username, ". Skipping...")
+            continue
+        followers = retrieve_sample_followers(uid)
+        
+        user_output_file = "data/" + username + ".txt"
+        f = open(user_output_file, "w") 
+        for u in followers:
+            f.write(u + "\n")
+        f.close()
+
+
 def main():
-    username_1 = "LilyPichu"
-    username_2 = "michaelreeves"
+    input_filename = "data/input_usernames.txt"
+    retrieve_input_followers(input_filename)
 
-    user1_id, user2_id = get_uids([username_1, username_2])
-
-    s = compare_followers(user1_id, user2_id)
-    print("degree of similarity between", username_1, "and", username_2, "is", s)
 
 if __name__ == "__main__":
     main()
